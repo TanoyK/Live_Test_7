e9c6a803b1b9a920a0d4d20406f4c7466777c86b
TanoyK <tanoyk.ju@gmail.com>
2023-06-19 23:59:29 +0600

    Test Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:live_test_7/secondscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,74 +8,96 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-
-      home: const MyHomePage(title: "Demo project"),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CounterApp> createState() => _CounterAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CounterAppState extends State<CounterApp> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int counter=0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-      title: Text("Live Test"),
-        
-    )
+        title: const Text("ProductList"),
+        centerTitle: true,
+        elevation: 5,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Text("Cointer Value:"),
+            Text("$counter"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                    flex: 50,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          counter = counter+1;
+                          if (counter >=17) {
+                            AlertDialog alert = AlertDialog(
+                              title: const Text("Counter Alert!"),
+                              content: Text("Counter value 5!"),
+                            );
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return alert;
+                              },
+                            );
+                          }
+
+                          if (counter>=10){
+                            Navigator.push(context,MaterialPageRoute(builder: (context) => MyHomePage(title: "Live Test"),));
+                          }
+
+
+                        });
+
+                      },
+                      child: Text("+",style: TextStyle(color: Colors.red),),
+                    )),
+                Flexible(
+                    flex: 25,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          counter = counter-1;
+                        });
+                      },
+                      child: Text("-",style: TextStyle(color: Colors.red),),
+                    ))
+              ],
+            )
+          ],
+
+        ),
+      ),
+
     );
   }
 }
